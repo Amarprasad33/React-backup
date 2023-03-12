89ae92976a2db5387c01bb5e7441dab8a89743d5
Amar Prasad <amarprasad77354@gmail.com>
2023-03-12 21:05:50 +0530

    Updated SortableTable till state implementation

---
+++ 06-comps/src/components/SortableTable.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Table from "./Table";
+
+function SortableTable(props) {
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
+
+    const { config } = props;
+
+    const handleClick = (label) => {
+        if(sortOrder === null){
+            setSortOrder('asc');
+            setSortBy(label)
+        } else if(sortOrder === 'asc'){
+            setSortOrder('desc');
+            setSortBy(label); 
+        } else if(sortOrder === 'desc'){
+            setSortOrder(null);
+            setSortBy(null);
+        }
+    }
+
+    const updatedConfig = config.map((column) => {
+        if(!column.sortValue) {
+            return column;
+        }
+
+        return{
+            ...column,
+            header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>
+        } 
+    })
+
+    // Only sort data if sortOrder && sortBy are not null 
+    // Make a copy of the 'data' prop, dont't modify the 'data' prop
+    // Find the correct sortValue function and use it for sorting
+
+
+    return (
+        <div>
+            {sortOrder} - {sortBy}
+            <Table {...props} config={updatedConfig} />
+        </div>  
+    );
+}
+
+export default SortableTable;
