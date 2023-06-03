827dd07dc70f05e5cad88477941c5715d7c843ce
Amar Prasad <amarprasad77354@gmail.com>
2023-06-03 23:57:02 +0530

    last changes

--- cars/src/components/CarValue.js
+++ cars/src/components/CarValue.js
@@ -2,21 +2,31 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 
-function CarValue () {
-  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
-    const filteredCars = data.filter((car) => {
-      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    let cost = 0;
-    for(let car of filteredCars){
-      cost += car.cost;
-    }
-    return cost;
-  });
+function CarValue() {
+  // const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+  //   const filteredCars = data.filter((car) => {
+  //     return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+  //   });
+  //   let cost = 0;
+  //   for(let car of filteredCars){
+  //     cost += car.cost;
+  //   }
+  //   return cost;
+  // });
+
+  // A better javascript way of doing above function is:
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) =>
+    data
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, car) => acc + car.cost, 0)
+  );
+
 
   return (
     <div>
-        Total Cost: ${totalCost}
+      Total Cost: ${totalCost}
     </div>
   )
 }
